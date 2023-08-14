33a7cae43dc1563061558dbd0bfb4a772d565a93
Sulaiman Abuqamar <sabuqamar82@gmail.com>
2023-08-14 13:21:38 +0300

    hi

--- DVA/basketball/views.py
+++ DVA/basketball/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from .forms import RegisterationForm
+from .forms import RegisterationForm, PlayerForm, TeamForm
+from .models import *
 
 # Create your views here.
 
@@ -20,9 +21,26 @@ def register(request):
     if request.POST:
         form = RegisterationForm(request.POST, request.FILES)
         print(request.FILES)
+        print(form)
+        # form.save()
         if form.is_valid():
-            print('Form is valid!')
-            form.save()
+            cleaned_data = form.cleaned_data
+            team_name = cleaned_data['team_name']
+            team_logo = cleaned_data['team_logo']
+            manager_fname = cleaned_data['manager_fname']
+            manager_lname = cleaned_data['manager_lname']
+            manager_number = cleaned_data['manager_number']
+            manager_email = cleaned_data['manager_email']
+            player_fname = cleaned_data['player_fname']
+            player_lname = cleaned_data['player_lname']
+            player_age = cleaned_data['player_age']
+            player_number = cleaned_data['player_number']
+            player_email = cleaned_data['player_email']
+            player_image = cleaned_data['player_image']
+            team = Team(team_name = team_name, team_logo = team_logo, manager_fname = manager_fname, manager_lname = manager_lname, manager_number = manager_number, manager_email = manager_email)
+            player = Player(player_fname = player_fname, player_lname = player_lname, player_age = player_age, player_number = player_number, player_email = player_email, player_image = player_image)
+            team.save()
+            player.save() 
         return redirect(schedule)
     return render(request, "Register.html", {'form' : RegisterationForm})
 
